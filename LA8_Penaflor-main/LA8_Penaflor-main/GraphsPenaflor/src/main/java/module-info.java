6c90ad2d5e59df3c2b7f4270d481ef564dc78859
AmielAdriem29 <130570841+AmielAdriem29@users.noreply.github.com>
2025-03-25 18:42:10 +0800

    Delete LA8_Penaflor-main/LA8_Penaflor-main/GraphsPenaflor directory

--- LA8_Penaflor-main/LA8_Penaflor-main/GraphsPenaflor/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.graphspenaflor {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.graphspenaflor to javafx.fxml;
-    exports com.example.graphspenaflor;
-}
